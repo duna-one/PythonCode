a34ae32a4c3f0704e8019d62fa960cfecdc52e19
Даниил <korotkovdan@gmail.com>
2021-10-23 15:43:16 +0300

    Added relatively secure data entry

--- AutomaticСontrolSystem.py
+++ AutomaticСontrolSystem.py
@@ -9,3 +9,34 @@ DELTA: Final = 1
 w = []
 v = []
 
+"""Data input area"""
+while(True):
+    print("Enter the number of iterations(more than 100)")
+    iterations_count = input()
+
+    if not iterations_count.isdecimal():
+        print("Incorrect input.")
+        continue
+
+    if int(iterations_count) < 100:
+        print("Need more than 100")
+        continue
+    else:
+        break
+
+while(True):
+    print("Use the standard values K1 and K2?(Y/N)")
+    inputValue = input()
+    if inputValue == 'Y':
+        print("Enter K1:")
+        K1 = int(input())
+        print("Enter K2:")
+        K2 = int(input())
+        break
+    elif inputValue == 'N':
+        print("The standard values K1=4 and K2=-0.2 are used")
+        break
+    else:
+        print("Incorrect input.")
+        continue
+"""End of data input area"""
