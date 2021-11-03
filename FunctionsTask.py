50a6a3b98c529db446d0aa156da8d45ac9b9631b
Даниил <korotkovdan@gmail.com>
2021-11-03 22:23:03 +0300

    Task 1 complited

--- FunctionsTask.py
+++ FunctionsTask.py
@@ -1,5 +1,20 @@
+import time
+
+"""Task 1"""
+def Delay(x):
+    print("Delay started\nStart time: " + time.ctime(time.time()))
+    time.sleep(x)
+    print("Delay ended\nEnd time: " + time.ctime(time.time()))
+
+print("TASK 1")
+
+print("Enter delay in seconds: ")
+x = int(input())
+Delay(x)
+"""//////////////////////////////////////////////////"""
+
 """Task 3"""
-print("Task 3 started")
+print("TASK 3")
 
 lambdaFuncResult = lambda b1, b2, X : b1 + b2 * X
 print("Enter b1:")
@@ -10,3 +25,4 @@ print("Enter X:")
 X = int(input())
 
 print("lambdaFuncResult: " + str(lambdaFuncResult(b1,b2,X)))
+"""//////////////////////////////////////////////////"""
