d559f01ec194aca2efcb13da6d005d79dd394950
Даниил <korotkovdan@gmail.com>
2021-11-09 21:56:32 +0300

    CodeFix

--- M2_B5.py
+++ M2_B5.py
@@ -7,8 +7,7 @@ print("Elements:\n" + str(list(elements)))
 """Get file name"""
 print("Please enter file name:")
 fileName = input()
-if not fileName.__contains__('.'): 
-    fileName = fileName + '.binary' #Add your own file extension if there is none
+fileName = fileName + '.bin' #Add file extension
 
 """Write binary file"""
 binFile = open(fileName, 'wb') #Open file to write
@@ -26,7 +25,7 @@ binFile = open(fileName, "rb") #Open file to read
 newElements = set() #New set
 for line in binFile:
     item = int(line) #Convert bytes to int
-    newElements = newElements.union({item}) #Add element to new set
+    newElements.add(item) #Add element to new set
 binFile.close() #Close file
 
 """Create elementsList"""
