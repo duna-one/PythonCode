67e8cad91cd87085ca38318213c870eeebc81097
Даниил <korotkovdan@gmail.com>
2021-12-13 23:09:25 +0300

    Filtered select done

--- DBFunctions.py
+++ DBFunctions.py
@@ -38,3 +38,62 @@ def ShowTable():
 
     cur.close()
     con.close()
+
+def GetByStatusAndArea():
+    con = sqlite3.connect("vuz.sqlite")
+    cur = con.cursor()
+
+    sql = """\
+    select distinct FedOkr FROM vuz_rf
+    """
+
+    FedOkr = []
+    for okr in cur.execute(sql).fetchall():
+        FedOkr.append(okr[0])
+
+    sql = """\
+    select distinct Status FROM vuz_rf
+    """
+
+    Status = []
+    for stat in cur.execute(sql).fetchall():
+        Status.append(stat[0])
+
+    #Выбор округа
+    print("Доступные федеральные округа: ")
+    for i in range(1, len(FedOkr)):
+        print("{}. {}".format(i, FedOkr[i-1]))
+
+    while True:
+        try:
+            SelectedOkr = int(input("Введите номер округа (или несколько через пробел): "))
+            if SelectedOkr in range(1, len(FedOkr)):
+                break;
+            else:
+                print("Некорректный ввод!")
+        except:
+            print("Введите число")
+
+    #Выбор статуса
+    print("Доступные статусы: ")
+    for i in range(1, len(Status)):
+        print("{}. {}".format(i, Status[i-1]))
+
+    while True:
+        try:
+            SelectedStatus = int(input("Введите номер округа: "))
+            if SelectedStatus in range(1, len(Status)):
+                break;
+            else:
+                print("Некорректный ввод!")
+        except:
+            print("Введите число")
+
+    sql = """\
+    SELECT z1full FROM vuz_rf JOIN vuzkart on vuz_rf.cod = vuzkart.codvuz
+    WHERE vuz_rf.status = '{}' AND vuz_rf.FedOkr = '{}'
+    """.format(Status[SelectedStatus-1], FedOkr[SelectedOkr-1])
+    
+    print("Результат:")
+    for resItem in cur.execute(sql).fetchall():
+        print(" - {}".format(resItem[0]))
--- MainUI.py
+++ MainUI.py
@@ -1,16 +1,18 @@
 import sys
 import DBFunctions as Db
 
-def MainUI():    
-    print("Меню:")
-    print("1. Отобразить таблицу")
-    print("0. Завершить программу")
+def MainUI():
+    while True:
+        print("Меню:")
+        print("1. Отобразить таблицу")
+        print("2. Отбор по статусу и федеральному округу")
+        print("0. Завершить программу")
 
-    work = True
-    while work:
         choice = input("Выберите пункт меню: ")
         if choice == "1":
             Db.ShowTable()
+        elif choice == "2":
+            Db.GetByStatusAndArea()
         elif choice == "0":
             sys.exit();
 
